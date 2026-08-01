c86ae1aa76883e5cb57d1b46daea065f8a9ab983
ernie <ernie-thi@thi.de>
2026-08-02 01:13:13 +0200

    stoic api agent

---
+++ python/langchain_tuts/main.py
@@ -0,0 +1,51 @@
+from langchain.chat_models import init_chat_model
+from deepagents import create_deep_agent
+from langchain.tools import tool
+
+import requests
+
+SYSTEM_PROMPT = """ You are a general agent for multiple tasks. Currently you are able to do:
+    - `get_stoicism_quote`: requests an api for stoic content
+    
+    If the request fails don't answer with quotes from yourself. Answer 'tool: {tool_name} failed miserably'
+
+    """
+
+@tool
+def get_stoicism_quote() -> dict:
+    """ Function to request stoic quote from api"""
+    url = 'https://stoic.tekloon.net/stoic-quote'
+    resp = requests.get(url, timeout=20)
+    
+    if resp.status_code == 200:
+        data = resp.json()
+        return data
+    else:
+        return  {"error":f"status {resp.status_code}"}
+
+
+def main():
+    local_model = init_chat_model("Qwythos-9B-Claude-Mythos-5-1M-MTP-Q6_K.gguf", 
+                            model_provider="openai", 
+                            base_url="http://localhost:8080/v1",
+                            api_key="none")
+
+    deep_agent = create_deep_agent(
+            model=local_model,
+            tools=[get_stoicism_quote],
+            system_prompt=SYSTEM_PROMPT
+            )
+
+    content = "Can you please give me a stoic quote"
+
+
+    deep_agent_result = deep_agent.invoke(
+    {"messages": [{"role": "user", "content": content}]},
+    config={"configurable": {"thread_id": "stoic-answers"}},
+    )   
+
+    print(deep_agent_result["messages"][-1].content)
+    
+
+if __name__ == "__main__":
+    main()
