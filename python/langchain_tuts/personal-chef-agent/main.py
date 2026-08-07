6851e3d256101ddc89dbd688224458328aa95b3a
ernie <ernie-thi@thi.de>
2026-08-07 17:05:30 +0200

    add basic personal chef agent

---
+++ python/langchain_tuts/personal-chef-agent/main.py
@@ -0,0 +1,66 @@
+from dotenv import load_dotenv
+from pathlib import Path
+
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain.messages import HumanMessage
+from typing import Dict, Any
+from exa_py import Exa
+
+
+system_prompt = """
+You are a personal chef. The user will either give you a list of ingredients they have available. Or an image of their fridge, which shows you what ingredients are available.
+
+Do not assume the user has any ingredients other than the list, e.g. if the user does not mention any vegetables or spices, they are not available.
+Using the web search tool, search the web for recipes that can be made with the ingredients they have.
+
+Return recipe suggestions and eventually the recipe instructions to the user, if requested.
+
+"""
+
+
+def encode_b64():
+    pass
+    #img_b64 = base64.b64encode(
+
+
+def env_setup():
+    api_path= Path('/home/ernie/.env')
+    load_dotenv(dotenv_path=api_path)
+
+@tool
+def web_search(query: str) -> Dict[str, Any]:
+    """Search the web for information"""
+    exa = Exa()
+    return exa.search(query, contents={"highlights": True})
+
+
+def main():
+    env_setup()
+    agent = create_agent(
+            model="gpt-5-nano",
+            tools=[web_search],
+            system_prompt=system_prompt,
+            checkpointer=InMemorySaver()
+            )
+    
+    config = {"configurable": {"thread_id": "1"}}
+    
+    question = HumanMessage(content=[
+        {"type": "text", "text": "What can i cook from chicken, rice and curry ?"},
+        ]
+        )
+
+    response = agent.invoke(
+            {"messages": [question]},
+            config
+            )
+
+    print(response['messages'][-1].content)
+
+
+
+
+if __name__ == "__main__":
+    main()
